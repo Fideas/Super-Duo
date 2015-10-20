4783b92562edd3948e9325221d70196d0dee1229
Nicolas Carrasco-Stevenson <niccarra@gmail.com>
2015-10-20 19:44:37 -0300

    Set barebone for implementing widget

---
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/TodayWidgetProvider.java
@@ -0,0 +1,16 @@
+package barqsoft.footballscores.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Provider for a widget that shows today matches
+ */
+public class TodayWidgetProvider extends AppWidgetProvider{
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
