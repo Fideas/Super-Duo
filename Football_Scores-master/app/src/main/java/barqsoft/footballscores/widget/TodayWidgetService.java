ecd348dad7affa8d01ba4f12fc695c0bd02b8491
Nicolas Carrasco-Stevenson <niccarra@gmail.com>
2015-10-21 11:27:58 -0300

    Add RemoteViewsService to the manifest

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/TodayWidgetService.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/TodayWidgetService.java
@@ -1,7 +1,17 @@
 package barqsoft.footballscores.widget;
 
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.widget.RemoteViewsService;
+
 /**
  * Created by Nicolás Carrasco on 21/10/2015.
  */
-public class TodayWidgetService {
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class TodayWidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return null;
+    }
 }
